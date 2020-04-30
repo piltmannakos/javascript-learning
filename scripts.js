66790a87c674c42d84c022ce3c5b7473d5f3f78e
piltmannakos <64487388+piltmannakos@users.noreply.github.com>
2020-04-30 18:00:24 +0200

    Adding p by clicking

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
 // Third part:
+console.log('Script file is loaded');
 
 $('h1').html('This is done by <em>JavaScript</em>');
-$('p').css('border', '1px solid red');
-$('p').css('padding', '10px');
 $('h2').toggleClass('blue');
 
+function myLittleFunction() {
+    $('main').append('<p>A p is appended in the main.<p>');
+    $('p').css('border', '1px solid red');
+    $('p').css('padding', '10px');
+}
+
+$('#add').click(myLittleFunction);
+$('#remove').click(function () {
+    $('p:last-child').remove();
+});
 
 
-console.log('Script file is loaded');
+
 
 // Single line comment
 
